9fc29901e4ae96de553fbe8f711e9f74f5c58d15
Paula Sochaczyńska <paula.weronika.es@gmail.com>
2020-05-20 15:43:39 +0200

    Test drawing and add drawing panel.

--- src/main/java/org/example/Launcher.java
+++ src/main/java/org/example/Launcher.java
@@ -2,8 +2,15 @@ package org.example;
 
 import org.example.game.Game;
 
+import javax.swing.*;
+
 public class Launcher {
     public static void main(String[] args) {
-        Game game = new Game();
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                Game game = new Game();
+            }
+        });
     }
 }
--- src/main/java/org/example/game/Game.java
+++ src/main/java/org/example/game/Game.java
@@ -4,11 +4,28 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Game extends JFrame {
+    private static final int MENU_BAR_HEIGHT = 25;
+    private static final int GAME_WIDTH = 800;
+    private static final int GAME_HEIGHT = 600 + MENU_BAR_HEIGHT;
+    private static final int RELATIVE_GAME_HEIGHT = GAME_HEIGHT - MENU_BAR_HEIGHT;
+
+    private static final int
+    private GraphicPanel panel = new GraphicPanel();
     public Game() throws HeadlessException {
         setVisible(true);
-        setSize(800, 600);
+        setSize(GAME_WIDTH, GAME_HEIGHT);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle("Labirynt");
+        add(panel);
+    }
+
+
+    class GraphicPanel extends JPanel{
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            g.drawLine(0, 0, GAME_WIDTH, RELATIVE_GAME_HEIGHT);
+        }
     }
 }
